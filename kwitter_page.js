ff22d461a8e8fb32d992a2e9e429ddc358e74618
Axtraix <98992939+Axtraix@users.noreply.github.com>
2022-05-21 19:26:59 -0500

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,15 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDsUL2rd3I56UzZ3Xn-ukV6ikkg-pUp-k4",
+    authDomain: "kwitter-f80f3.firebaseapp.com",
+    databaseURL: "https://kwitter-f80f3-default-rtdb.firebaseio.com",
+    projectId: "kwitter-f80f3",
+    storageBucket: "kwitter-f80f3.appspot.com",
+    messagingSenderId: "995110437224",
+    appId: "1:995110437224:web:cb3c2b988d35d6173116dd"
+  };
+
+
+function login(){
+    document.getElementById("inputbox")
+}
